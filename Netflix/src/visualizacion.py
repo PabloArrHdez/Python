f805b724cced4dafe5d42246d7b92e634ae7981c
Pablo <pabloarrhdez@gmail.com>
2025-03-02 12:43:00 +0100

    modified:   Netflix/src/visualizacion.py

--- Netflix/src/visualizacion.py
+++ Netflix/src/visualizacion.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def grafico_barras_1 (df_netflix_2):
+def peliculas_genero (df_netflix_2):
     total_genero = df_netflix_2['genre'].value_counts() 
     plt.figure(figsize=(9, 4))  
     total_genero.plot(kind='bar')
@@ -8,4 +8,24 @@ def grafico_barras_1 (df_netflix_2):
     plt.xticks(rotation=45, ha="right") 
     plt.ylabel('Cantidad')
     plt.savefig("imagenes/nº_peliculas_genero.png")
+    plt.show();
+
+def filmografia_tipo (df_netflix_2):
+    total_tipo = df_netflix_2['type'].value_counts()
+    plt.figure(figsize=(9, 4))
+    total_tipo.plot(kind='bar')
+    plt.xlabel('Tipo')
+    plt.xticks(rotation=45, ha="right") 
+    plt.ylabel('Cantidad')
+    plt.savefig("imagenes/nº_filmografia_tipo.png")
+    plt.show();
+
+def filmografia_año(df_netflix_2):
+    filmografia = df_netflix_2['date_added_year'].value_counts().sort_index() 
+    plt.figure(figsize=(8, 5)) 
+    plt.plot(filmografia.index, filmografia.values, marker='o', linestyle='-', color='b') #
+    plt.xlabel('Años') 
+    plt.ylabel('Cantidad') 
+    plt.grid(True)
+    plt.savefig("imagenes/filmografia_año.png")
     plt.show();
